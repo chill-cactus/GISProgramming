78916e8d53cc95bbd907c6a4f2f3865f836727dc
chill-cactus <michaela.wojslaw@gmail.com>
2023-09-13 12:47:06 -0500

    Lab02_Code

---
+++ Lab02/Lab02_Code.py
@@ -0,0 +1,33 @@
+#GEOG 392- Lab 2
+#Name: Michaela Wojslaw
+#Course: GEOG 392
+#Instructor: Dr. Zhang
+#TA: Ms. Binbin Lin
+#UIN: 933004203
+#Date: September 13, 2023
+#Statement: I state this is my assignment and I have completed it with the assistance of the TA in lab.
+
+#Question 1
+'''
+part1= [1,2,4,8,16,32,64,128,256,512,1024,2048,4096]
+a_result=1
+for m in part1:
+    a_result= a_result * m
+print(a_result)
+
+
+#Question 2
+part2= [-1,23,483,8573,-13847,-381569,1652337,718522177]
+my_result=0
+for j in part2:
+       my_result= my_result + j
+print(my_result)
+'''
+
+#Question 3
+part3=[146,875,911,83,81,439,44,5,46,76,61,68,1,14,38,26,21]
+some_result=0
+for i in part3:
+    if i%2==0:
+        some_result= some_result + i
+print(some_result)
